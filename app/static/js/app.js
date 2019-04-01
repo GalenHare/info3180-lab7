2a1509eaae634d6640c7ad9101c20f7345f404e5
GalenHare <galenhare@hotmail.com>
2019-03-31 22:25:30 -0500

    Final?

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -12,6 +12,9 @@ Vue.component('app-header', {
           <li class="nav-item active">
             <router-link class="nav-link" to="/">Home <span class="sr-only">(current)</span></router-link>
           </li>
+          <li>
+            <router-link class="nav-link" to="/upload">Upload <span class="sr-only">(current)</span></router-link>
+          </li>
         </ul>
       </div>
     </nav>
@@ -99,7 +102,8 @@ const router = new VueRouter({
     routes: [
         {path: "/", component: Home},
         // Put other routes here
-        {path: "/api/upload", component: uploadForm},
+
+        {path: "/upload", component: uploadForm},
         // This is a catch all route in case none of the above matches
         {path: "*", component: NotFound}
     ]
